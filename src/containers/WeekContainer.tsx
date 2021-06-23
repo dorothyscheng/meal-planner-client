534b0eed5a3e82e5b170979ffc14585c0bdb6752
dcheng <cheng.dorothy.s@gmail.com>
2021-06-23 15:52:56 -0600

    Changed week display to dropdown on user dash

--- src/containers/WeekContainer.tsx
+++ src/containers/WeekContainer.tsx
@@ -9,9 +9,9 @@ interface Props {
 const WeekContainer = (props: Props): JSX.Element => {
     const [selectedWeek, setSelectedWeek] = useState(props.weeks? props.weeks[0] : null);
 
-    const handleWeekClick = (e: React.MouseEvent): void => {
-        const target = e.target as Element;
-        const weekId = target.getAttribute('id');
+    const handleWeekClick = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+        // const target = e.target as Element;
+        const weekId = e.target.value;
         if (props.weeks) {
             setSelectedWeek(props.weeks.filter(week => week._id === weekId)[0]);
         }
@@ -28,7 +28,7 @@ const WeekContainer = (props: Props): JSX.Element => {
         });
         weekTitles = props.weeks.map(week => {
             return (
-                <h3 className="list-label" key={week._id} id={week._id} onClick={handleWeekClick}>{week.name}</h3>
+                    <option className="list-label" key={week._id} value={week._id}>{week.name}</option>
             )
         })
     }
@@ -36,7 +36,7 @@ const WeekContainer = (props: Props): JSX.Element => {
     return (
         <section className="dash-section">
             <div className="week-titles">
-                {props.weeks ? weekTitles : <h3>Loading...</h3>}
+                {props.weeks ? <select name="week" onChange={handleWeekClick}>{ weekTitles }</select> : <h3>Loading...</h3>}
             </div>
             <WeekShow week={selectedWeek} />
         </section>
